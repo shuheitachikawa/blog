3a51e697184605d156eb5b446e32a56779dfd571
shuhei.tachikawa <shuhei.tusx@gmail.com>
2021-03-23 17:30:07 +0900

    a

--- src/components/categories.tsx
+++ src/components/categories.tsx
@@ -20,10 +20,10 @@ export const Categories: VFC = () => {
         {categories.map((category) => (
           <div
             key={category.href}
-            className={`mr-5 cursor-pointer ${routeName !== category.href && "opacity-60"} duration-300`}
+            className={`sm:mr-5 mr-3 cursor-pointer ${routeName !== category.href && "opacity-60"} duration-300`}
           >
             <Link href={category.href}>
-              <div className={`sm:w-20 w-16 rounded-full overflow-hidden`}>
+              <div className="sm:w-20 w-16 rounded-full overflow-hidden">
                 <img src={category.name} alt=""/>
                 {/* <Icon size={70} href={category.name} /> */}
               </div>
