039d1f611e2fc2dc027f2dcd5c47ab4bb4439ec7
tachikawa <tachikawa_s@beartail.jp>
2021-09-26 04:20:25 +0900

    s

--- src/components/aside.tsx
+++ src/components/aside.tsx
@@ -1,5 +1,5 @@
 import type { VFC } from "react";
-// import { Icon } from "components/Icon";
+import { Icon } from "components/Icon";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const links = [
@@ -21,7 +21,7 @@ export const Aside: VFC = () => {
   return (
     <aside className="bg-sub text-white shadow-lg rounded-md p-6 sm:max-w-aside sticky top-8 mt-10 sm:mt-0 mx-4">
       <div className="flex items-center mb-4">
-        {/* <Icon size={70} href="/cat.jpg" /> */}
+        <Icon size={70} href="/cat.jpg" />
         <h2 className="text-xl ml-3 font-bold">Bubekiti</h2>
       </div>
       <p className="text-sm leading-5 mb-6">
