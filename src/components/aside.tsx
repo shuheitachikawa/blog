38902a85468b5ba5dc8c5d18ee524a55913330ab
tachikawa <tachikawa_s@beartail.jp>
2021-09-26 04:52:17 +0900

    s

--- src/components/aside.tsx
+++ src/components/aside.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Icon } from "components/Icon";
+import { Icon } from "src/components/Icon";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const links = [
