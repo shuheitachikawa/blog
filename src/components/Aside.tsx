7ce6914e5fd8501fdb0e10979ed027005e8fa4fa
tachikawa <tachikawa_s@beartail.jp>
2021-09-26 05:11:32 +0900

    s

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -34,9 +34,9 @@ export const Aside: React.FC = () => {
       <div className="flex justify-center">
         {links.map((link) => {
           return (
-            <div key={link.name} className="w-8 mr-6 last:mr-0">
-              <a href={link.href} target="_blank" aria-label={`${link.name}へ`} rel="noopener" className='w-full'>
-                <FontAwesomeIcon icon={["fab", link.name as any]} className='w-full' />
+            <div key={link.name} className="w-10 mr-6 last:mr-0">
+              <a href={link.href} target="_blank" aria-label={`${link.name}へ`} rel="noopener">
+                <FontAwesomeIcon icon={["fab", link.name as any]} />
               </a>
             </div>
           );
