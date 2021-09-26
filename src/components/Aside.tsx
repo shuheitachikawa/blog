8d2b17f87fb2aa1393d0704aa4d374d9881322e0
tachikawa <tachikawa_s@beartail.jp>
2021-09-26 12:57:11 +0900

    s

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -20,27 +20,29 @@ const links = [
 export const Aside: React.FC = () => {
   return (
     <aside className="bg-sub text-white shadow-lg rounded-md p-6 sm:max-w-aside sticky top-8 mt-10 sm:mt-0 mx-4">
-      <div className="flex items-center mb-4">
-        <Icon size={70} href="/cat.jpg" />
-        <h2 className="text-xl ml-3 font-bold">Bubekiti</h2>
-      </div>
-      <p className="text-sm leading-5 mb-6">
-        都内のWebエンジニアです。
-        <br />
-        Saas企業のフロントエンドエンジニアをしつつ、スタートアップのお手伝いや、個人開発をしたりしています。
-        <br />
-        食べることが好きです。
-      </p>
-      <div className="flex justify-center">
-        {links.map((link) => {
-          return (
-            <div key={link.name} className="mr-6 last:mr-0">
-              <a href={link.href} target="_blank" aria-label={`${link.name}へ`} rel="noopener">
-                <FontAwesomeIcon icon={["fab", link.name as any]} width={26} />
-              </a>
-            </div>
-          );
-        })}
+      <div className="">
+        <div className="flex items-center mb-4">
+          <Icon size={70} href="/cat.jpg" />
+          <h2 className="text-xl ml-3 font-bold">Bubekiti</h2>
+        </div>
+        <p className="text-sm leading-5 mb-6">
+          都内のWebエンジニアです。
+          <br />
+          Saas企業のフロントエンドエンジニアをしつつ、スタートアップのお手伝いや、個人開発をしたりしています。
+          <br />
+          食べることが好きです。
+        </p>
+        <div className="flex justify-center">
+          {links.map((link) => {
+            return (
+              <div key={link.name} className="mr-6 last:mr-0">
+                <a href={link.href} target="_blank" aria-label={`${link.name}へ`} rel="noopener">
+                  <FontAwesomeIcon icon={["fab", link.name as any]} width={26} />
+                </a>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </aside>
   );
--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -14,7 +14,7 @@ export const Posts: React.FC<Props> = ({ posts }) => {
       {posts.map((blog) => (
         <li key={blog.id} className="select-none border-t border-sub hover:bg-sub transition cursor-pointer mb-5 sm:mb-0">
           <Link href={`/${blog.id}`}>
-            <div className="sm:flex sm:items-center sm:py-5 py-3 px-2 sm:px-0 sm:pl-4">
+            <div className="sm:flex sm:items-center sm:py-5 py-3 px-2 sm:px-0 sm:pl-4 ">
               {/* PC画像 */}
               <div className="hidden sm:flex mr-4 min-w-listImage items-center">
                 <Image src={`${blog.image.url}`} alt={blog.title} width={140} height={140 / 1.618} objectFit={"cover"} />
